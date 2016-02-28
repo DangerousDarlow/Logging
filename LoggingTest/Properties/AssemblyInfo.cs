0a4623fce8d21aae2a8a3fc73207c05dc3f0a9e3
Nick Darlow <dangerousdarlow@users.noreply.github.com>
2016-02-28 14:49:41 +0000

    Assembly info updates

--- LoggingTest/Properties/AssemblyInfo.cs
+++ LoggingTest/Properties/AssemblyInfo.cs
@@ -4,21 +4,24 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("LoggingTest")]
 [assembly: AssemblyDescription("Simple debug logging framework test")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LoggingTest")]
-[assembly: AssemblyCopyright("Copyright © Nick Darlow 2015")]
+[assembly: AssemblyCopyright("Copyright © Nick Darlow 2015 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("95529a78-23f6-47dd-baeb-7bc45895a948")]
 
 // Version information for an assembly consists of the following four values:
@@ -30,4 +33,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.3.*")]
