9547ca0554ece73e1df8f3327cc062e6603f17df
Nick Darlow <dangerousdarlow@users.noreply.github.com>
2016-03-20 11:12:54 +0000

    Updated logging calls in example app

--- ExampleLoggingApp/Program.cs
+++ ExampleLoggingApp/Program.cs
@@ -5,17 +5,25 @@ namespace ExampleLoggingApp
 {
   internal class Program
   {
+    // Logging message 3
+    private static void Fn() => Logger.Log(LogLevel.Error, "id-str3");
+
+
     private static void Main()
     {
       var logEncoder = new XmlLogEncoder();
       var byteWriter = new LazyStreamByteWriter(FileStreamFactory.CreateApplicationDataFileStream);
       Logger.AddLogWriter(new Lazy<ILogWriter>(() => new LogWriter(logEncoder, byteWriter)));
 
-      Logger.Log(LogLevel.Error, "id-str");
+      // Logging message 1
+      Logger.Log(LogLevel.Error, "id-str1");
 
       const string param = "param-str";
 
-      Logger.Log(LogLevel.Error, "id-str", param, null);
+      // Logging message 2
+      Logger.Log(LogLevel.Error, "id-str2", param, null);
+
+      Fn();
     }
   }
 }
